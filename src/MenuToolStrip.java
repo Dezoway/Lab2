486950da185e31c0d0230a1d75fcda1b048f9a97
Dezoway <kostya.trepalin.2013@mail.ru>
2023-04-10 18:13:02 +0300

    Added icons in MenuStripTool

--- src/MenuToolStrip.java
+++ src/MenuToolStrip.java
@@ -1,16 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 import java.security.Key;
 
 public class MenuToolStrip {
     private String style="Solid";   //Стиль пера, по умолчанию Solid
+    private Color brushColor;   //Цвет кисти
     private final JMenuBar bar;
     public MenuToolStrip(){
          bar = new JMenuBar();
         JMenu file = new JMenu("File");
         JMenu edit = new JMenu("Edit");
         JMenu help = new JMenu("Help");
+        brushColor = new Color(0,0,0);
         createFileSubMenu(file);
         createEditSubMenu(edit);
         createHelpSubMenu(help);
@@ -23,9 +26,13 @@ public class MenuToolStrip {
     }
     private void createFileSubMenu(JMenu file){
         JMenuItem newF = new JMenuItem("New");
+        newF.setIcon(new ImageIcon(new ImageIcon("src/resources/NoteBook.png").getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT)));
         JMenuItem open = new JMenuItem("Open");
+        open.setIcon(new ImageIcon(new ImageIcon("src/resources/folder.png").getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT)));
         JMenuItem save = new JMenuItem("Save");
+        save.setIcon(new ImageIcon(new ImageIcon("src/resources/save.png").getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT)));
         JMenuItem exit = new JMenuItem("Exit");
+        exit.setIcon(new ImageIcon(new ImageIcon("src/resources/exit.png").getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT)));
         newF.setAccelerator(KeyStroke.getKeyStroke('N', KeyEvent.CTRL_DOWN_MASK));
         open.setAccelerator(KeyStroke.getKeyStroke("F3"));
         save.setAccelerator(KeyStroke.getKeyStroke("F2"));
@@ -37,14 +44,16 @@ public class MenuToolStrip {
     }
     private void createEditSubMenu(JMenu edit){
         JMenuItem undo = new JMenuItem("Undo");
-        JMenuItem reno = new JMenuItem("Reno");
-        //JMenuItem pen = new JMenuItem("Pen");
+        undo.setIcon(new ImageIcon(new ImageIcon("src/resources/Undo.jpg").getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT)));
+        JMenuItem redo = new JMenuItem("Redo");
+        redo.setIcon(new ImageIcon(new ImageIcon("src/resources/Redo.png").getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT)));
         JMenu pen = new JMenu("Pen");
+        pen.setIcon(new ImageIcon(new ImageIcon("src/resources/Pen.png").getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT)));
         undo.setAccelerator(KeyStroke.getKeyStroke('Z', KeyEvent.CTRL_DOWN_MASK));
-        reno.setAccelerator(KeyStroke.getKeyStroke('Z',KeyEvent.CTRL_DOWN_MASK+KeyEvent.SHIFT_DOWN_MASK));
+        redo.setAccelerator(KeyStroke.getKeyStroke('Z',KeyEvent.CTRL_DOWN_MASK+KeyEvent.SHIFT_DOWN_MASK));
         setPenSubMenu(pen);
         edit.add(undo);
-        edit.add(reno);
+        edit.add(redo);
         edit.add(pen);
     }
     private void setPenSubMenu(JMenu pen){
@@ -71,12 +80,17 @@ public class MenuToolStrip {
         style.add(dot);
         style.add(ddd);
         JMenuItem color = new JMenuItem("Color");
+        color.setIcon(new ImageIcon(new ImageIcon("src/resources/Palitra.jpg").getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT)));
+        color.addActionListener(e->{
+            MenuToolStrip.this.brushColor = JColorChooser.showDialog(JFrame.getFrames()[0],"Цвет",new Color(0,0,0));
+        });
         pen.add(style);
         pen.add(color);
 
     }
     private void createHelpSubMenu(JMenu help){
         JMenuItem about = new JMenuItem("About");
+        about.setIcon(new ImageIcon(new ImageIcon("src/resources/About.jpg").getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT)));
         about.setAccelerator(KeyStroke.getKeyStroke("F1"));
         help.add(about);
     }
